7c4165d6e0b3e5a1ca63af7e86da47601f3eee06
Vi <viwang@Vis-MacBook-Pro.local>
2023-10-13 11:04:04 +0100

    Adds continuous sampling of ngram

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
 from typing import List
+import json
+from enum import Enum
+from random import random
+
 from ngram import NGram, NGramStore
 from intervals import (
     get_key_invariant_ngram,
@@ -64,17 +67,12 @@ def count_ngams_in_form(
         ngram_string = ""
         next_chord = ""
         if key_invariant:
-            # TODO - we don't properly handle chord bracketing yet - try catch to just ignore these for now
+            # TODO - we don't properly handle chords in parenthesis yet (optional chors )- try catch to just ignore these for now
             try:
                 ngram_chords = (
                     ngram_queue[queue_pointer:] + ngram_queue[0:queue_pointer]
                 )
                 relative_root = get_chord_root(ngram_chords[0])
-
-                ngram_string = get_key_invariant_ngram(
-                    ngram_chords,
-                    relative_root=relative_root,
-                )
                 next_chord = get_relative_chord_name(wrapped_form[i + n], relative_root)
             except:
                 continue
@@ -119,9 +117,103 @@ def query_key_invariant_ngram(store: NGramStore, chord_names: List[str]):
         )
 
 
+class SamplingMethod(Enum):
+    MOST_FREQUENT = 1
+    UNFIROM_SAMPLE = 2
+    TEMPERATURE = 3
+
+
+class NgramSampler:
+    n: int
+    chords: [str]
+    key_invariant_ngram: bool
+    ngram_store: NGramStore
+    sampling_method: SamplingMethod
+
+    def __init__(
+        self,
+        n: int,
+        initial_chords: [str],
+        ngram_store: NGramStore = None,
+        key_invariant_ngram=True,
+        sampling_method=SamplingMethod.MOST_FREQUENT,
+    ):
+        self.n = n
+        self.chords = initial_chords
+        self.key_invariant_ngram = key_invariant_ngram
+        self.sampling_method = sampling_method
+
+        if not ngram_store:
+            self.ngram_store = count_ngrams(key_invariant=key_invariant_ngram, n=n)
+
+    def sample_next(self) -> str:
+        # String for ngram chords, will either be concreate or invariant
+        window = ""
+        # Only used for key invariant
+        relative_root = None
+
+        if not self.key_invariant_ngram:
+            window = "".join(self.chords[-self.n :])
+        else:
+            window = get_key_invariant_ngram(
+                self.chords[-self.n :], relative_root=relative_root
+            )
+            relative_root = get_chord_root(self.chords[-self.n])
+
+        # print(self.ngram_store.store)
+        # print(window)
+        # print(self.chords)
+
+        # TODO how to handle key error? we aren't keeping a vocab so can't really do +1 sampling
+        ngram = self.ngram_store.store[window]
+        counts = ngram.get_sorted_counts()
+        chord = ""
+
+        if self.sampling_method == SamplingMethod.MOST_FREQUENT:
+            chord = counts[0][0]
+
+        elif self.sampling_method == SamplingMethod.UNFIROM_SAMPLE:
+            random_val = random()
+            chord_set = False
+
+            for potential_chord, count in counts:
+                chord_proportion = count / ngram.count
+                if chord_proportion > random_val:
+                    chord = potential_chord
+                    chord_set = True
+                    break
+                else:
+                    random_val -= chord_proportion
+
+            if not chord_set:
+                # If random val got to end of counts, return the last count
+                chord = counts[-1][0]
+
+        if self.key_invariant_ngram:
+            chord = get_concrete_chord_name(chord, relative_root=relative_root)
+
+        self.chords.append(chord)
+        return chord
+
+    def sample_n(self, n):
+        pass
+
+
 if __name__ == "__main__":
-    store = count_ngrams(key_invariant=True, n=2)
-    query_key_invariant_ngram(store, ["Dm7", "G7"])
-    print()
-    store2 = count_ngrams(key_invariant=True, n=3)
-    query_key_invariant_ngram(store2, ["Cmaj7", "Ebmaj7", "Abmaj7"])
+    # store = count_ngrams(key_invariant=True, n=2)
+    # query_key_invariant_ngram(store, ["Dm7", "G7"])
+    # print()
+    # store2 = count_ngrams(key_invariant=True, n=3)
+    # query_key_invariant_ngram(store2, ["Cmaj7", "Ebmaj7", "Abmaj7"])
+    # # store = count_ngrams(key_invariant=True, n=2)
+    sampler = NgramSampler(
+        3,
+        ["Dm7", "G7", "C6"],
+        key_invariant_ngram=False,
+        sampling_method=SamplingMethod.UNFIROM_SAMPLE,
+    )
+
+    for i in range(20):
+        print(sampler.sample_next())
+
+    print(sampler.chords)
